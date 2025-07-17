98db228a70758dfdc4e9dabe634d3751f8e028ed
harsha1636 <harsha60201@gmail.com>
2025-07-17 17:20:47 +0530

    Configured the required entity for Employees with related fields and methods

---
+++ src/main/java/com/Payment/Zappay/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.Payment.Zappay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String firstName;
+    private String LastName;
+    private String email;
+}
