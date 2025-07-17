e885bf86e7c6e2e368381571d2897db3a981a2ea
harsha1636 <harsha60201@gmail.com>
2025-07-17 19:17:01 +0530

    This is the manual access way to get an instance of the mapper when you're not using Spring.

---
+++ src/main/java/com/Payment/Zappay/mapper/EmployeeMapper.java
@@ -0,0 +1,33 @@
+package com.Payment.Zappay.mapper;
+
+import com.Payment.Zappay.dto.EmployeeDto;
+import com.Payment.Zappay.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+
+//        public static EmployeeDto mapToEmployeeDto(Employee employee){
+//            return new EmployeeDto(
+//                    employee.getId(),
+//                    employee.getFirstName(),
+//                    employee.getLastName(),
+//                    employee.getEmail()
+//            );
+//        }
+//        public static Employee mapToEmployee(EmployeeDto employeeDto){
+//            return new Employee(
+//                    employeeDto.getId(),
+//                    employeeDto.getFirstName(),
+//                    employeeDto.getLastName(),
+//                    employeeDto.getEmail()
+//            );
+//        }
+
+    EmployeeMapper employeeMapper = Mappers.getMapper(EmployeeMapper.class);
+    EmployeeDto mapToEmployeeDto(Employee employee);
+    Employee mapToEmployee(EmployeeDto employeeDto);
+
+}
