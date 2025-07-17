fcbc212be7d05b8fb14e27ead987d5a7f32e327d
harsha1636 <harsha60201@gmail.com>
2025-07-17 16:00:50 +0530

    Initialize Spring Boot project with basic structure

---
+++ src/main/java/com/Payment/Zappay/ZappayApplication.java
@@ -0,0 +1,13 @@
+package com.Payment.Zappay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZappayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZappayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Payment/Zappay/ZappayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Payment.Zappay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZappayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
