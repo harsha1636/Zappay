feba0189080a16a88cb58e203f23071125ca20fe
harsha1636 <harsha60201@gmail.com>
2025-07-17 22:26:44 +0530

    Created service methods for endpoints

---
+++ src/main/java/com/Payment/Zappay/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.Payment.Zappay.service;
+
+import com.Payment.Zappay.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+    void deleteEmployee(Long employeeId);
+}
